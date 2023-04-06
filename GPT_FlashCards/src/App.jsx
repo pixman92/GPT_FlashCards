8436d42d9290ccdc7e00a86e1ee586afa08935d4
Christian B <gotrustd@verizon.net>
2023-04-06 01:29:39 -0700

    created REPL with textarea

--- GPT_FlashCards/src/App.jsx
+++ GPT_FlashCards/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
-import MyFirebaseFunctions from './MyFirebaseFunctions'
+import FirebaseStuff from './FirebaseStuff'
+import { FirebaseError } from 'firebase/app'
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <div>
-      <MyFirebaseFunctions />
+      <FirebaseStuff />
 
     </div>
   )
